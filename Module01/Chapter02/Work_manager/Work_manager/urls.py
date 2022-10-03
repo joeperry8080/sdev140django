80988c692aad47b5018f03d8ab1a2fad28183e38
Joe Perry <jperry5@ivytech.edu>
2022-10-03 10:53:22 -0400

    Edits to satisfy version updates of Django.

--- Module01/Chapter02/Work_manager/Work_manager/urls.py
+++ Module01/Chapter02/Work_manager/Work_manager/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import include, url
+from django.urls import include, re_path
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'Work_manager.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+    #url(r'^admin/', include(admin.site.urls)),
+    re_path(r'^admin/', admin.site.urls),
+]
